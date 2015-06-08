0186e8babfd7665cb156159f25285ef3455cdcaf
Peter Edström <peter@peteredstrom.se>
2015-06-08 11:12:08 +0200

    Test

--- ekonomi/settings.py
+++ ekonomi/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'ekonomi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        #'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
